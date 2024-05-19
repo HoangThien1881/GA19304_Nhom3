aff8b438e6bde7864bbeb36ac4dfbfe2e4a36340
HoangThien1881 <163240345+HoangThien1881@users.noreply.github.com>
2024-05-19 14:45:12 +0700

    aaa

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,29 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        //aa
-    }
+    Vector2 moveInput;
 
-    // Update is called once per frame
+  
     void Update()
     {
         
     }
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log("Move input: " + moveInput);
+        //moveInput 
+        //1,0 -> move phải
+        //-1,0 -> move trái
+        // 0,1 -> lên
+        //0,-1 -> xuống
+    }
+    void OnJump()
+    {
+        Debug.Log("Jump");
+    }
+
 }
