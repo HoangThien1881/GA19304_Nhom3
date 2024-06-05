8055e77a405c1f71b77b37286f4c3f4eaa9be3e8
thuan05052005 <momspread2005@gmail.com>
2024-06-06 00:38:28 +0700

    ha

--- Assets/Scripts/Isekai.cs
+++ Assets/Scripts/Isekai.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Isekai : MonoBehaviour
 {
@@ -14,5 +15,12 @@ public class Isekai : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 }
